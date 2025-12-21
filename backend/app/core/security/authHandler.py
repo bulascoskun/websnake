737c1c032c6a747f987bce24a09af1ac4aed675a
Ulaş Coşkun <ulas_coskun99@hotmail.com>
2025-12-21 03:12:07 +0300

    Implement JWT authentication and password hashing utilities

---
+++ backend/app/core/security/authHandler.py
@@ -0,0 +1,27 @@
+import jwt
+from decouple import config
+from typing import cast
+import time
+
+JWT_SECRET = cast(str, config("JWT_SECRET"))
+JWT_ALGORITHM = cast(str, config("JWT_ALGORITHM", default="HS256"))
+
+
+class AuthHandler(object):
+    @staticmethod
+    def sign_jwt(user_id : int):
+        payload = {
+            "user_id": user_id,
+            "expires": time.time() + 3600
+        }
+
+        token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+        return token
+    
+    @staticmethod
+    def decode_jwt(token : str) -> dict:
+        try:
+            decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+            return decoded_token if decoded_token["expires"] >= time.time() else None
+        except:
+            print("Unable to decode the token")
