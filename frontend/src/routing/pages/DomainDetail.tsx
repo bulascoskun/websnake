63cd66c741f8de5acc1d8939cbd63a35ad4557c0
Ulaş Coşkun <ulas_coskun99@hotmail.com>
2025-12-25 15:33:25 +0300

    up

--- frontend/src/routing/pages/DomainDetail.tsx
+++ frontend/src/routing/pages/DomainDetail.tsx
@@ -1,21 +1,101 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+
+import type { BundledLanguage } from '@/components/ui/shadcn-io/code-block';
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
+  CodeBlock,
+  CodeBlockBody,
+  CodeBlockContent,
+  CodeBlockCopyButton,
+  CodeBlockFilename,
+  CodeBlockFiles,
+  CodeBlockHeader,
+  CodeBlockItem,
+  CodeBlockSelect,
+  CodeBlockSelectContent,
+  CodeBlockSelectItem,
+  CodeBlockSelectTrigger,
+  CodeBlockSelectValue,
+} from '@/components/ui/shadcn-io/code-block';
+const code = [
+  {
+    language: 'jsx',
+    filename: 'MyComponent.jsx',
+    code: `function MyComponent(props) {
+  return (
+    <div>
+      <h1>Hello, {props.name}!</h1>
+      <p>This is an example React component.</p>
+    </div>
+  );
+}`,
+  },
+  {
+    language: 'tsx',
+    filename: 'MyComponent.tsx',
+    code: `function MyComponent(props: { name: string }) {
+  return (
+    <div>
+      <h1>Hello, {props.name}!</h1>
+      <p>This is an example React component.</p>
+    </div>
+  );
+}`,
+  },
+];
 
 const DomainDetail = () => {
   return (
     <Card>
       <CardHeader>
         <CardTitle></CardTitle>
-        {/* <CardAction>
-          <Button>Add Domain</Button>
-        </CardAction> */}
       </CardHeader>
-      <CardContent></CardContent>
+      <CardContent>
+        <CodeBlock data={code} defaultValue={code[0].language}>
+          <CodeBlockHeader>
+            <CodeBlockFiles>
+              {(item) => (
+                <CodeBlockFilename key={item.language} value={item.language}>
+                  {item.filename}
+                </CodeBlockFilename>
+              )}
+            </CodeBlockFiles>
+            <CodeBlockSelect>
+              <CodeBlockSelectTrigger>
+                <CodeBlockSelectValue />
+              </CodeBlockSelectTrigger>
+              <CodeBlockSelectContent>
+                {(item) => (
+                  <CodeBlockSelectItem
+                    key={item.language}
+                    value={item.language}
+                  >
+                    {item.language}
+                  </CodeBlockSelectItem>
+                )}
+              </CodeBlockSelectContent>
+            </CodeBlockSelect>
+            <CodeBlockCopyButton
+              onCopy={() => console.log('Copied code to clipboard')}
+              onError={() => console.error('Failed to copy code to clipboard')}
+            />
+          </CodeBlockHeader>
+          <CodeBlockBody>
+            {(item) => (
+              <CodeBlockItem key={item.language} value={item.language}>
+                <CodeBlockContent
+                  language={item.language as BundledLanguage}
+                  themes={{
+                    light: 'vitesse-light',
+                    dark: 'vitesse-dark',
+                  }}
+                >
+                  {item.code}
+                </CodeBlockContent>
+              </CodeBlockItem>
+            )}
+          </CodeBlockBody>
+        </CodeBlock>
+      </CardContent>
     </Card>
   );
 };
